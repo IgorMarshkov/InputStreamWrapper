7857a0bfd0ce985815a6b6a018d7292539760703
IgorMarshkov <marshkov@gmail.com>
2014-08-12 12:27:07 +0300

    Add runner class with logger

---
+++ src/main/java/com/ism/Runner.java
@@ -0,0 +1,13 @@
+package com.ism;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Runner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("---GO---");
+    }
+}
