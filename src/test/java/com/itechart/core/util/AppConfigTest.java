6468c419f44023c412754eb8e3cf1de0bf016452
IgorMarshkov <marshkov@gmail.com>
2014-08-14 14:30:23 +0300

    Add additional asserts to AppConfig test

--- src/test/java/com/itechart/core/util/AppConfigTest.java
+++ src/test/java/com/itechart/core/util/AppConfigTest.java
@@ -1,13 +1,20 @@
 package com.itechart.core.util;
 
+import com.itechart.core.model.Bandwidth;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class AppConfigTest extends Assert {
 
     @Test
     public void testParsePeriod() {
         AppConfig config = AppConfig.getInstance();
-        assertNotEquals(config.getBandwidthPeriods(), "");
+        String periodsStr = config.getBandwidthPeriods();
+        assertNotEquals(periodsStr, "");
+
+        List<Bandwidth> bandwidths = BandwidthUtil.parsePeriod(periodsStr);
+        assertEquals(bandwidths.get(0).getFromTime(), bandwidths.get(bandwidths.size() - 1).getToTime());
     }
 }
