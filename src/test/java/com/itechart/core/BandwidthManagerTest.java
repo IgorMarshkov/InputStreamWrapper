2b8c498c139ebec41dda8887fb54e26739ae780d
IgorMarshkov <marshkov@gmail.com>
2014-08-14 16:16:01 +0300

    Add BandwidthManagerTest

--- src/test/java/com/itechart/core/BandwidthManagerTest.java
+++ src/test/java/com/itechart/core/BandwidthManagerTest.java
@@ -1,20 +1,27 @@
 package com.itechart.core;
 
+import com.itechart.core.model.Bandwidth;
+import org.joda.time.LocalTime;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class BandwidthManagerTest extends Assert {
 
-
-
-    @AfterTest
-    public void clean() {
-
-    }
-
     @Test
     public void testBandwidthManager() {
+        BandwidthManager bandwidthManager = BandwidthManager.getInstance();
+        assertNotEquals(bandwidthManager.getActiveBandwidth(), null);
+        assertEquals(bandwidthManager.getAvgBandwidth(), bandwidthManager.getActiveBandwidth().getBandwidth());
 
+        Bandwidth bandwidth = new Bandwidth();
+        bandwidth.setFromTime(new LocalTime(1, 0, 0));
+        bandwidth.setToTime(new LocalTime(10, 0, 0));
+        bandwidth.setBandwidth(64000);
+        bandwidthManager.setActiveBandwidth(bandwidth);
+        assertEquals(bandwidthManager.getActiveBandwidth() , bandwidth);
+        assertEquals(bandwidthManager.getAvgBandwidth(), bandwidth.getBandwidth() / 1);
+
+        bandwidthManager.recalculateAvgBandwidth(3);
+        assertEquals(bandwidthManager.getAvgBandwidth(), bandwidth.getBandwidth() / 3);
     }
 }
