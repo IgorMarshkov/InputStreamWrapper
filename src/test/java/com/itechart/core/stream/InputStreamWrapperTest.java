44fdad5fae4ef6fa16fb99541fbbee3cd80d7ccf
IgorMarshkov <marshkov@gmail.com>
2014-08-15 15:46:46 +0300

    Add test InputStreamWrapper

--- src/test/java/com/itechart/core/stream/InputStreamWrapperTest.java
+++ src/test/java/com/itechart/core/stream/InputStreamWrapperTest.java
@@ -1,12 +1,28 @@
 package com.itechart.core.stream;
 
+import com.itechart.core.BandwidthManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class InputStreamWrapperTest extends Assert {
 
     @Test
-    public void testInputStreamWrapper() {
+    public void testInputStreamWrapper() throws IOException {
+        String bandwidthPeriods = "12:00am-02:23pm=|02:23pm-11:00pm=|11:00pm-12:00am=";
+        InputStreamWrapper isw = new InputStreamWrapper(this.getClass().getResourceAsStream("/test.txt"), bandwidthPeriods);
+        double avgBandwidth = BandwidthManager.getInstance().getAvgBandwidth();
 
+        try {
+            while ((isw.read()) != -1) {
+                boolean isOk = (isw.getBytesPerSec() - avgBandwidth) < (avgBandwidth / 1000);
+                assertEquals(isOk, true);
+            }
+        } finally {
+            if (isw != null) {
+                isw.close();
+            }
+        }
     }
 }
