d5bb86320012b44983d07a7f4cda5504312f9850
ArseniRadziuk <arseni.radziuk@itechart-group.com>
2014-08-15 11:02:19 +0300

    logging

--- src/main/java/com/itechart/Runner.java
+++ src/main/java/com/itechart/Runner.java
@@ -9,39 +9,70 @@ import java.io.*;
 public class Runner {
     private static final Logger LOGGER = LoggerFactory.getLogger(Runner.class);
 
-    public static void main(String[] args) {
-        InputStreamWrapper is = null;
-        OutputStream os = null;
+    private class Task implements Runnable {
+        private final Logger LOGGER = LoggerFactory.getLogger(Task.class);
+        private final InputStream inputStream;
+        private OutputStream outputStream;
 
-        try {
-            is = new InputStreamWrapper(new Runner().getClass().getResourceAsStream("/test.txt"), "12:00am-02:23pm=100|02:23pm-11:00pm=100|11:00pm-12:00am=");
-            os = new FileOutputStream("/new_source.txt");
+        public Task(final InputStream inputStream, OutputStream outputStream) {
+            this.inputStream = inputStream;
+            this.outputStream = outputStream;
 
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while((bytesRead = is.read(buffer)) !=-1){
-                os.write(buffer, 0, bytesRead);
-               // System.out.println(new String(buffer));
-            }
-
-            //os.flush();
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage(), e);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException e) {
-                    LOGGER.error(e.getMessage(), e);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        LOGGER.info(inputStream.toString());
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            LOGGER.error(e.getMessage(), e);
+                        }
+                    }
                 }
-            }
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (IOException e) {
-                    LOGGER.error(e.getMessage(), e);
+            }).start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    if (outputStream != null) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+            } catch (IOException e) {
+                LOGGER.debug(e.getMessage(), e);
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                }
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
                 }
             }
         }
+    }
+
+    private void go() {
+        InputStreamWrapper wrapper1 = new InputStreamWrapper(this.getClass().getResourceAsStream("/test.txt"));
+        InputStreamWrapper wrapper2 = new InputStreamWrapper(this.getClass().getResourceAsStream("/test.txt"));
+
+        new Thread(new Task(wrapper1, null)).start();
+        new Thread(new Task(wrapper2, null)).start();
+    }
+
+    public static void main(String[] args) {
+        new Runner().go();
     }
 }
