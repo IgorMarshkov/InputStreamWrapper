7aa9f0f8472759bbd658918fcbdd0201426bfee6
IgorMarshkov <marshkov@gmail.com>
2014-08-18 10:48:52 +0300

    Fix test InputStreamWrapperTest

--- src/test/java/com/itechart/core/stream/InputStreamWrapperTest.java
+++ src/test/java/com/itechart/core/stream/InputStreamWrapperTest.java
@@ -11,9 +11,9 @@ public class InputStreamWrapperTest extends Assert {
     @Test
     public void testInputStreamWrapper() throws IOException {
         String bandwidthPeriods = "12:00am-02:23pm=|02:23pm-11:00pm=|11:00pm-12:00am=";
-        double avgBandwidth = BandwidthManager.getInstance().getAvgBandwidth();
 
         try (InputStreamWrapper isw = new InputStreamWrapper(this.getClass().getResourceAsStream("/test.txt"), bandwidthPeriods)) {
+            double avgBandwidth = BandwidthManager.getInstance().getAvgBandwidth();
             while ((isw.read()) != -1) {
                 boolean isOk = (isw.getBytesPerSec() - avgBandwidth) < (avgBandwidth / 1000);
                 assertEquals(isOk, true);
